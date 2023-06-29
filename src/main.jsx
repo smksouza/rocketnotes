01603dda3a34accedff27108eaa6dcb3090c9983
Samuel Souza <smksouza.dev@gmail.com>
2023-06-29 19:19:49 -0300

    pagina signIn Up e profile finalizadas

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/global';
 import theme from './styles/theme';
-import { Home } from './pages/Home';
+import { Profile } from './pages/Profile';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <Profile />
     </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,36 @@
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi";
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
+import { Container, Form, Avatar } from "./styles";
+
+export function Profile() {
+  return (
+    <Container>
+      <header>
+        <a href="/">
+          <FiArrowLeft />
+        </a>
+      </header>
+      <Form>
+        <Avatar>
+          <img src="https://github.com/smksouza.png" alt="Foto do usuário" />
+          <label htmlFor="avatar">
+            <FiCamera />
+            <input 
+            id="avatar"
+            type="file"
+            />
+          </label>
+        </Avatar>
+        <Input placeholder="nome" type="text" icon={FiUser} />
+        <Input placeholder="E-nail" type="text" icon={FiMail} />
+        <Input placeholder="Senha Atual" type="password" icon={FiLock} />
+        <Input placeholder="Nova senha" type="password" icon={FiLock} />
+
+        <Button title="Salvar" />
+      </Form>
+    </Container>
+  );
+}
