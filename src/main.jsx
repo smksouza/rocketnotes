948bad7dd289a8104061b431d816ee4aa0f5871f
Samuel Souza <smksouza.dev@gmail.com>
2023-07-04 21:51:52 -0300

    refatoração e adição do hooks

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GlobalStyles from './styles/global';
-import { ThemeProvider } from 'styled-components';
+import React from "react";
+import ReactDOM from "react-dom";
+import GlobalStyles from "./styles/global";
+import { ThemeProvider } from "styled-components";
 
-import { Routes } from './routes';
+import { AuthProvider } from "./hooks/auth";
 
-import theme from './styles/theme';
+import { Routes } from "./routes";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import theme from "./styles/theme";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Routes />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
-
