e21c6e5a6626df26ef1276fc3a63d7b67e8c2321
Samuel Souza <smksouza.dev@gmail.com>
2023-07-14 16:57:31 -0300

    função de atulizar dados da conta no banco de dados

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -32,6 +32,22 @@ function AuthProvider({ children }) {
     setData({});
   }
 
+  async function updateProfile({ user }) {
+    try {
+      await api.put("/users", user);
+      localStorage.setItem("@rocketnotes:user", JSON.stringify(user));
+
+      setData({ user, token: data.token });
+      alert("Perfil atualizado!");
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert("Não foi possível atualizar o perfil.");
+      }
+    }
+  }
+
   useEffect(() => {
     const token = localStorage.getItem("@rocketnotes:token");
     const user = localStorage.getItem("@rocketnotes:user");
@@ -50,8 +66,9 @@ function AuthProvider({ children }) {
     <AuthContext.Provider
       value={{
         signIn,
-        user: data.user,
         signOut,
+        updateProfile,
+        user: data.user,
       }}
     >
       {children}
