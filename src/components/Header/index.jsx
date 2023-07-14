47420e006457c7658d32f52fd539445dc4fc6edd
Samuel Souza <smksouza.dev@gmail.com>
2023-07-14 17:21:37 -0300

    atualizando header com a info do usuario

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,20 @@
 import { RiShutDownLine } from "react-icons/ri";
 import { useAuth } from "../../hooks/auth";
 import { Container, Profile, Logout } from "./styles";
+import { api } from "../../services/api";
 
 export function Header() {
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
+  const avatarUrl = user.avatar
+    ? `${api.defaults.baseURL}/files/${user.avatar}`
+    : avatarPlaceholder;
   return (
     <Container>
       <Profile to="/profile">
-        <img src="https://github.com/smksouza.png" alt="Foto do usuário" />
+        <img src={avatarUrl} alt={user.name} />
         <div>
           <span>Bem-vindo</span>
-          <strong>Samuel Souza</strong>
+          <strong>{user.name}</strong>
         </div>
       </Profile>
 
